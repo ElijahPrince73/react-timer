c0250c8ae6eda70df6e3b458fac05db6a9ec4000
Elijah Prince <elijahprince73@gmail.com>
2024-07-14 20:26:10 -0700

    first commit

---
+++ src/countDown.jsx
@@ -0,0 +1,32 @@
+
+import React, { useState} from 'react';
+import { useCountDown } from "./useCountDown";
+
+const ExpiredNotice = () => {
+  return (
+    <div className="expired-notice">
+      <span>Expired!!!</span>
+      <p>Please select a future date and time.</p>
+    </div>
+  );
+};
+
+const ShowCounter = ({ days, hours, minutes, seconds }) => {
+  return (
+    <div className="show-counter">
+      {days} {hours} {minutes} {seconds}
+    </div>
+  );
+};
+
+const CountDownTimer = ({ targetDate }) => {
+  const [days, hours, minutes, seconds] = useCountDown(targetDate);
+
+  if (days + hours + minutes + seconds <= 0) {
+    return <ExpiredNotice />;
+  } else {
+    return <ShowCounter days={days} hours={hours} minutes={minutes} seconds={seconds} />;
+  }
+};
+
+export default CountDownTimer;
